8a4e0234d72af0ab1a50c4fc4e07cb7f279c66a2
Omurbek1 <omurbekmamytbekov545@gmail.com>
2022-05-29 16:35:42 +0600

    server

--- #11server/#1crserver/app.js
+++ #11server/#1crserver/app.js
@@ -1,9 +1,27 @@
-const http = require('http');
-http.createServer(function (request, response) {
-    console.log("Url:" + request.url);
-    console.log("Type request:" + request.method);
-    console.log("User-Agent:" + request.headers["user - agent"]);
-    console.log(request.headers);
+// Эта функция принимает два параметра:
+// request: хранит информацию о запросе
+// response: управляет отправкой ответа
 
+// const http = require('http');
+// http.createServer(function (request, response) {
+//     console.log("Url:" + request.url);
+//     console.log("Type request:" + request.method);
+//     console.log("User-Agent:" + request.headers["user - agent"]);
+//     console.log(request.headers);
+
+//     response.end()
+// }).listen(3000)
+
+
+
+
+
+// Request
+const http = require('http');
+
+http.createServer(function (request, response) {
+    response.setHeader("UserId", 12);
+    response.setHeader("Content-Type", "text/html");
+    response.write("<h2>Hello World</h2>")
     response.end()
 }).listen(3000)
