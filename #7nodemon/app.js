83fe6ca600b15fb605d2572b6aa5eb749a27f54e
Omurbek1 <omurbekmamytbekov545@gmail.com>
2022-05-29 15:27:30 +0600

    Ассинхронность

--- #7nodemon/app.js
+++ #7nodemon/app.js
@@ -1,18 +1,36 @@
-const { callbackify } = require("util");
+// const { callbackify } = require("util");
 
-// Async Node js
-function dispaly(data, callback) {
-    var randInt = Math.random() * (10 - 1) + 1;
-    var err = randInt > 5 ? new Error('Иштетуудо ката чыкты. 5тен чон сан!') : null
-    setTimeout(function () {
-        callback(err, data);
+// // Async Node js
+// function dispaly(data, callback) {
+//     var randInt = Math.random() * (10 - 1) + 1;
+//     var err = randInt > 5 ? new Error('Иштетуудо ката чыкты. 5тен чон сан!') : null
+//     setTimeout(function () {
+//         callback(err, data);
 
-    }, 0)
+//     }, 0)
+// }
+
+// console.log("Программа иштеп баштады!");
+// dispaly("Маалымат иштелип жатат....", function (err, data) {
+//     if (err) throw err;
+//     console.log(data);
+// });
+// console.log("Программа иштелип бутту!");
+
+
+// method 2
+function displayInf(callbacks) {
+    callbacks();
 }
+console.log("Программа иштеп баштады!!!");
+setTimeout(function () {
+    console.log("timeout 500");
+}, 500)
+setTimeout(function () {
+    console.log('timeout 100');
+}, 100)
 
-console.log("Программа иштеп баштады!");
-dispaly("Маалымат иштелип жатат....", function (err, data) {
-    if (err) throw err;
-    console.log(data);
-});
+displayInf(function () {
+    console.log('timeoutтан тышкары');
+})
 console.log("Программа иштелип бутту!");
