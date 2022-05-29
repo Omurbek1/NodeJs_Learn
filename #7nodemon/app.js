68744c546edf895a162b3d6cae6ef5872cd3a976
Omurbek1 <omurbekmamytbekov545@gmail.com>
2022-05-29 15:14:49 +0600

    Ассинхронность

---
+++ #7nodemon/app.js
@@ -0,0 +1,7 @@
+// Async Node js
+function displayInfo(data) {
+    console.log(data);
+}
+console.log('Прогармманын башталышы!');
+displayInfo('Маалымат иштелип жатат....!')
+console.log("Программа иштеп бутту");
