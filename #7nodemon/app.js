3c814efb21884604386eb2ad5b6e34f7681ba67c
Omurbek1 <omurbekmamytbekov545@gmail.com>
2022-05-29 15:22:00 +0600

    Ассинхронность

--- #7nodemon/app.js
+++ #7nodemon/app.js
@@ -1,7 +1,18 @@
+const { callbackify } = require("util");
+
 // Async Node js
-function displayInfo(data) {
-    console.log(data);
+function dispaly(data, callback) {
+    var randInt = Math.random() * (10 - 1) + 1;
+    var err = randInt > 5 ? new Error('Иштетуудо ката чыкты. 5тен чон сан!') : null
+    setTimeout(function () {
+        callback(err, data);
+
+    }, 0)
 }
-console.log('Прогармманын башталышы!');
-displayInfo('Маалымат иштелип жатат....!')
-console.log("Программа иштеп бутту");
+
+console.log("Программа иштеп баштады!");
+dispaly("Маалымат иштелип жатат....", function (err, data) {
+    if (err) throw err;
+    console.log(data);
+});
+console.log("Программа иштелип бутту!");
