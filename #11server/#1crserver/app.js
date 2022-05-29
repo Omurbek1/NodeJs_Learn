5d3bc48250a5d0ffcdd78fac32b808195f3f56d1
Omurbek1 <omurbekmamytbekov545@gmail.com>
2022-05-29 16:59:10 +0600

    server-marsgurtizasiya

--- #11server/#1crserver/app.js
+++ #11server/#1crserver/app.js
@@ -17,17 +17,40 @@
 
 
 // Request
-const http = require('http');
+// const http = require('http');
 
+// http.createServer(function (request, response) {
+//     response.setHeader("Content-Type", "text/html")
+//     response.write("<DOCTYPE html>");
+//     response.write("<html>")
+//     response.write("<head>")
+//     response.write("<title>Hello Node Js</title>")
+//     response.write("<meta charset=\"utf-8\">")
+//     response.write("</head")
+//     response.write("<body><h2>Салам</h2></body>")
+//     response.write("</html>")
+//     response.end()
+// }).listen(3000)
+
+
+
+// Маршуртизация
+
+const http = require('http');
 http.createServer(function (request, response) {
-    response.setHeader("Content-Type", "text/html")
-    response.write("<DOCTYPE html>");
-    response.write("<html>")
-    response.write("<head>")
-    response.write("<title>Hello Node Js</title>")
-    response.write("<meta charset=\"utf-8\">")
-    response.write("</head")
-    response.write("<body><h2>Салам</h2></body>")
-    response.write("</html>")
+    response.setHeader("Content-Type", "text/html; charset=utf-8");
+
+    if (response.url === "/home" || request.url === '/') {
+        response.write("<h2>Home</h2>")
+    }
+    else if (request.url === "/about") {
+        response.write("<h2>About</h2>")
+    }
+    else if (request.url == "/contact") {
+        response.write("<h2>Contact</h2>")
+    }
+    else {
+        response.write("<h2>Not Found!</h2>")
+    }
     response.end()
 }).listen(3000)
