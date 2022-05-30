cf31145d9eaf825d6a9d29dfbe25513a739283e9
Omurbek1 <omurbekmamytbekov545@gmail.com>
2022-05-30 12:41:59 +0600

    express

--- 12express/#1express1/app.js
+++ 12express/#1express1/app.js
@@ -8,8 +8,21 @@ const app = express();
 // Маршуртизация учун обратботчикти аныктайбыз
 app.get("/", function (request, response) {
     // Жоопту жиберебиз
-    response.send("<h1>Салам баарынарга! Мен Express окуп жатам!</h1>")
+    response.send("<h1>Башкы бет!</h1>")
 
 });
+// Маршуртизация биз жонундо
+app.get("/about", function (request, response) {
+    // Жоопту жиберебиз
+    response.send("<h1>Биз жонундо!</h1>")
+
+});
+// Маршуртизация контактар
+app.get("/contact", function (request, response) {
+    // Жоопту жиберебиз
+    response.send("<h1>Контактар!</h1>")
+
+});
+
 // Портту укканды баштайбызя
 app.listen(3000)
