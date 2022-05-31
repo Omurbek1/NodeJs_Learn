3db7d26b0054cce05f6adf9bcd185a5cd3e50980
Omurbek1 <omurbekmamytbekov545@gmail.com>
2022-05-31 12:16:58 +0600

    представлениеидвижок

--- 12express/#8handlebars/app.js
+++ 12express/#8handlebars/app.js
@@ -2,8 +2,15 @@ const express = require('express');
 const app = express();
 app.set("view engine", "hbs");
 app.use("/contact", function (_, response) {
-    response.render("contact.hbs");
+    response.render("contact.hbs", {
+        title: "Менин контактарым";
+        email: "omurbek@gmail.com";
+        
+    });
 });
+app.use("/about", function (_, response) {
+    response.send("Биз жонундо")
+})
 app.use("/", function (_, response) {
     response.send("Башкы бет")
 })
