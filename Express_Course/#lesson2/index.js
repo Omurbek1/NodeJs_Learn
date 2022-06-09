950b55ae3250284326e82827492e1ef81f5f873e
Omurbek1 <omurbekmamytbekov545@gmail.com>
2022-06-09 12:08:02 +0600

    expresscourse

--- Express_Course/#lesson2/index.js
+++ Express_Course/#lesson2/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 
 const app = express()
+
+app.get('/', (req,res) => {
+    
+})
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`server has been started on port ${PORT}`);
