9f74ad30c5688dcc076e1b04b8d8fd9144bd7786
Omurbek1 <omurbekmamytbekov545@gmail.com>
2022-06-09 12:29:12 +0600

    expresscourse

--- Express_Course/#lesson2/index.js
+++ Express_Course/#lesson2/index.js
@@ -2,21 +2,26 @@ const express = require('express');
 const path = require('path');
 const app = express()
 
-app.get('/', (req, res) => {
-    // res.send('<h1>Hello  sExpress</h1>')
-    res.sendFile(path.resolve(__dirname, 'static', 'index.html'))
-})
 
-app.get('/contact', (req, res) => {
-    // res.send('<h1>Hello  sExpress</h1>')
-    res.sendFile(path.resolve(__dirname, 'static', 'contact.html'))
-})
+app.use(express.static(path.resolve(__dirname, 'static')))
+
+// app.get('/', (req, res) => {
+//     // res.send('<h1>Hello  sExpress</h1>')
+//     res.sendFile(path.resolve(__dirname, 'static', 'index.html'))
+// })
+
+// app.get('/contact', (req, res) => {
+//     // res.send('<h1>Hello  sExpress</h1>')
+//     res.sendFile(path.resolve(__dirname, 'static', 'contact.html'))
+// })
 
 // Бул бетти кочуруу
-app.get('/download', (req, res) => {
-  
-    res.download(path.resolve(__dirname, 'static', 'index.html'))
-})
+// app.get('/download', (req, res) => {
+
+//     res.download(path.resolve(__dirname, 'static', 'index.html'))
+// })
+
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`server has been started on port ${PORT}`);
