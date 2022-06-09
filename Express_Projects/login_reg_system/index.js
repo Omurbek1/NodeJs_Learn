75ea3bbda208b8d140e0166f0a6979d95ca7b991
Omurbek1 <omurbekmamytbekov545@gmail.com>
2022-06-09 15:11:06 +0600

    login_register_system

--- Express_Projects/login_reg_system/index.js
+++ Express_Projects/login_reg_system/index.js
@@ -2,9 +2,16 @@ const express = require('express');
 const ejs = require('ejs');
 const PORT = process.env.PORT || 3000;
 const app = express()
+const bodyParser = require('body-parser');
+
+const mongoose = require('mongoose');
+const User = require('./models/Users')
+mongoose.connect("mongodb://localhost:27017/usersDB")
+
 
 app.use(express.static("public"))
 app.set("view engine", "ejs")
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get("/", (req, res) => {
     res.render("index")
@@ -20,5 +27,23 @@ app.get("/login", (req, res) => {
 
 
 // Ports request are here
+app.post('/register', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const newUser = new User({
+        email: email,
+        password: password
+    });
+    newUser.save((err) => {
+        err ? console.log(err) : res.send('Successfull');
+    })
+
+})
+
+app.post('/login', (req, res) => {
+
+})
+
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
--- Express_Projects/login_reg_system/models/Users.js
+++ Express_Projects/login_reg_system/models/Users.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost:27017/usersDB")
+
 
 const userSchema = new mongoose.Schema({
     email: String,
