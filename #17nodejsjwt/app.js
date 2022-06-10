ef2121abb38330f9a64482c02925ab677529afbb
Omurbek1 <omurbekmamytbekov545@gmail.com>
2022-06-10 17:04:59 +0600

    jwt

--- #17nodejsjwt/app.js
+++ #17nodejsjwt/app.js
@@ -11,6 +11,23 @@ app.get('/', async (req, res, next) => {
 
 })
 
+
+app.use(async (req, res, next) => {
+    const error = new Error("Not Found")
+    error.status = 404
+    next(error)
+})
+
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500)
+    res.send({
+        error: {
+            status: err.status || 500,
+            message: err.message
+        }
+    })
+})
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
