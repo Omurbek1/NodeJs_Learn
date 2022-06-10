70a888434be86931d4736be2e0314dd6bd02d922
Omurbek1 <omurbekmamytbekov545@gmail.com>
2022-06-10 12:14:50 +0600

    node-jwt

--- Express_Projects/auth_express_jwt/index.js
+++ Express_Projects/auth_express_jwt/index.js
@@ -1,13 +1,14 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 5000
 const app = express()
 
-
-const start = () => {
+app.use(express.json())
+const start = async () => {
     try {
-        app.listen(PORT, () => console.log('Server started'))
+        await mongoose.connect(`mongodb + srv://oma:oma52280930@cluster0.kt7z4.mongodb.net/auth_roles?retryWrites=true&w=majority`)
+        app.listen(PORT, () => console.log(`Сервер ${PORT} да иштеп жатат!`))
     } catch (e) {
         console.log(e);
 
