c7021aa81dd95857f4ba410acbd76db8aae7033d
Omurbek1 <omurbekmamytbekov545@gmail.com>
2022-06-10 15:08:39 +0600

    mongo-delete-update

--- Express_Projects/auth_express_jwt/controllers/books.js
+++ Express_Projects/auth_express_jwt/controllers/books.js
@@ -11,3 +11,40 @@ exports.createBook = async (req, res) => {
     await book.save()
     res.send({ data: book })
 }
+
+exports.findBook = async (req, res) => {
+
+    try {
+        const book = await Book.findById(req.params.id);
+        res.send({ data: book });
+    } catch {
+        res.status(404).send({ error: "Book is not found " })
+    }
+
+}
+
+// update
+exports.updateBook = async (req, res) => {
+
+    try {
+        const book = await Book.findById(req.params.id);
+        Object.assign(book, req.body)
+        book.save();
+        res.send({ data: book });
+    } catch {
+        res.status(404).send({ error: "Book is not found " })
+    }
+}
+
+
+// delete
+
+exports.deleteBook = async (req, res) => {
+    try {
+        const book = await Book.findById(req.params.id);
+        await book.remove();
+        res.send({ data: true });
+    } catch {
+        res.status(404).send({ error: "Book is not found " })
+    }
+}
--- Express_Projects/auth_express_jwt/index.js
+++ Express_Projects/auth_express_jwt/index.js
@@ -17,6 +17,10 @@ mongoose.connect("mongodb+srv://admin:admin@cluster0.kt7z4.mongodb.net/auth_role
         const PORT = process.env.PORT || 5000
         app.get('/books', bookController.findBooks);
         app.post('/books', bookController.createBook);
+        app.get('/book/:id', bookController.findBook)
+        app.patch('/books/:id', bookController.updateBook)
+        app.delete('/book/:id', bookController.deleteBook)
+
         app.listen(PORT, () => {
             console.log(`Сервер ${PORT} да иштеп жатат!!! Connection database`);
         })
