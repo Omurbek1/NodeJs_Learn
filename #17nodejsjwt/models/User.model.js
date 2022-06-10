23b536adbef35e5de2f3da4c6c5787c00d059424
Omurbek1 <omurbekmamytbekov545@gmail.com>
2022-06-10 19:23:44 +0600

    jwt

--- #17nodejsjwt/models/User.model.js
+++ #17nodejsjwt/models/User.model.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-
+const bcrypt = require('bcrypt')
 const Schema = mongoose.Schema
 
 const userSchema = new Schema({
@@ -11,10 +11,30 @@ const userSchema = new Schema({
     },
     password: {
         type: String,
-        required:true
+        required: true
+    }
+})
+
+
+userSchema.pre('save', async function (next) {
+    try {
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(this.password, salt);
+        this.password = hashedPassword
+        next()
+    } catch (error) {
+        next(error)
+    }
+})
+
+userSchema.post('save', async function (next) {
+    try {
+        console.log('Called after saving a user')
+    } catch (error) {
+        next(error)
     }
 })
 
 const User = mongoose.model('user', userSchema)
-module.exports=User
+module.exports = User
 
