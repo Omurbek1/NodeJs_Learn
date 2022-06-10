ba88d4b35d1a6c25405defdaf4fb3009a98485a8
Omurbek1 <omurbekmamytbekov545@gmail.com>
2022-06-10 19:06:35 +0600

    chekauth

--- #17nodejsjwt/Routes/Auth.route.js
+++ #17nodejsjwt/Routes/Auth.route.js
@@ -2,22 +2,26 @@ const express = require('express');
 const router = express.Router()
 const createError = require('http-errors')
 const User = require('../models/User.model')
+const { authSchema } = require('../helper/validation.schema')
 
 router.post('/register', async (req, res, next) => {
     console.log(req.body);
     try {
         const { email, password } = req.body
-        if (!email || !password) throw createError.BadRequest()
-        const doesExits = await User.findOne({ email: email })
-        if (doesExits) throw createError.Conflict(`${email} is already is registered!`)
+        // if (!email || !password) throw createError.BadRequest()
+        const result = await authSchema.validateAsync(req.body)
 
-        const user = new User({ email, password })
+        const doesExits = await User.findOne({ email: result.email })
+        if (doesExits) throw createError.Conflict(`${result.email} is already is registered!`)
+
+        const user = new User(result)
 
         const savedUser = await user.save()
 
 
         res.send(savedUser)
     } catch (error) {
+        if (error.isJoi === true) error.status = 422
         next(error)
     }
 })
