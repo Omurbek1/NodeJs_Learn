6d3be91b658103576d075f7716b8ba65012cd33f
Omurbek1 <omurbekmamytbekov545@gmail.com>
2022-06-10 12:50:20 +0600

    node-jwt

--- Express_Projects/auth_express_jwt/index.js
+++ Express_Projects/auth_express_jwt/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const Book = require('./models/Book')
 mongoose.connect(`mongodb+srv://admin:admin@cluster0.kt7z4.mongodb.net/auth_roles`, {
     useNewUrlParser: true
 })
@@ -8,8 +9,10 @@ mongoose.connect(`mongodb+srv://admin:admin@cluster0.kt7z4.mongodb.net/auth_role
         const app = express()
 
         const PORT = process.env.PORT || 5000
-        app.get('/', (req, res) => {
-            res.send('<h1>Updated </h1>')
+        app.get('/books', async (req, res) => {
+            const books = await Book.find();
+
+            res.send({ data: books })
         })
 
         app.listen(PORT, () => {
