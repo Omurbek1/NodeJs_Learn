ac990b2df633d922084b23651f71631cdd057a1d
Omurbek1 <omurbekmamytbekov545@gmail.com>
2022-06-10 17:35:13 +0600

    jwt

--- #17nodejsjwt/helper/init_mongodb.js
+++ #17nodejsjwt/helper/init_mongodb.js
@@ -1,7 +1,25 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://admin:admin@cluster0.kt7z4.mongodb.net/auth_roles', { dbName: 'auth_roles' })
+mongoose.connect('mongodb+srv://admin:admin@cluster0.kt7z4.mongodb.net/auth_roles',
+    {
+        dbName: 'auth_roles',
+        useNewUrlParser: true
+    },
+)
     .then(() => {
         console.log('Mongodb connected!!');
     })
     .catch((err) => console.log(err.message))
+
+
+mongoose.connection.on('connected', () => {
+    console.log('Mongoose connected to db');
+})
+
+mongoose.connection.on('error', (err) => {
+    console.log(err.message)
+})
+
+mongoose.connection.on('disconnected', () => {
+    console.log('Mongoose connection is disconnected!!');
+})
