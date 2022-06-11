ca219fd24d8a259606be007f5bf27cd5061579b6
Omurbek1 <omurbekmamytbekov545@gmail.com>
2022-06-11 18:28:11 +0600

    login

--- #17nodejsjwt/helper/jwt_helper.js
+++ #17nodejsjwt/helper/jwt_helper.js
@@ -8,15 +8,18 @@ module.exports = {
             const payload = {
 
             }
-            const secret =process.env.ACCESS_TOKEN_SECRET
+            const secret = process.env.ACCESS_TOKEN_SECRET
             const options = {
                 expiresIn: "1d",
                 issuer: "super.com",
                 audience: userId
             }
             JWT.sign(payload, secret, options, (err, token) => {
-                if (err) rejected(err)
-                resolve(token)
+                if (err) {
+                    console.log(err.message)
+
+                    rejected(createError.InternalServerError())
+                }
             })
         })
     }
