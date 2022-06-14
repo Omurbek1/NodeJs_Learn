3f51abd2e626848da794f8ac8aeec7350f1f7aa6
Omurbek Mamytbekov <omurbekmamytbekov545@gmail.com>
2022-06-14 19:23:18 +0600

    router

--- Express_Course/#4lesson_routes/routes/user.js
+++ Express_Course/#4lesson_routes/routes/user.js
@@ -4,22 +4,38 @@ router.get('/', (req, res) => {
     res.setDefaultEncoding('User name');
 });
 
-// КФЫСКАРТЫЛЫП ЖАЗЫЛГАНЫ
-ROU
-router.get('/new', (req, res) => {
-    res.send('New User')
-})
+// КЫСКАРТЫЛЫП ЖАЗЫЛГАНЫ
 
-router.post('/', (req, res) => {
-    res.send('Create User')
-})
+router
+    .route('/:id').get((req, res) => {
+        res.send(`Get user with id ${req.params.id}`)
+    }).delete((req, res) => {
+        res.send(`Delete user with id ${req.params.id}`)
+    })
+    .put((req, res) => {
+        res.send(`Update  user with id ${req.params.id}`)
+    })
 
-router.get('/:id', (req, res) => {
-    res.send(`Get user with id ${req.params.id}`)
-})
 
-router.delete('/:id', (req, res) => {
-    res.send(`Delete user with id ${req.params.id}`)
-})
-router.put('/:id', (req, res) => {
-    res.send(`Update  user with id
+// router.post((req, res) => {
+//     res.send('Create User')
+// })
+
+// router.get('/:id', (req, res) => {
+//     res.send(`Get user with id ${req.params.id}`)
+// })
+
+// router.delete('/:id', (req, res) => {
+//     res.send(`Delete user with id ${req.params.id}`)
+// })
+// router.put('/:id', (req, res) => {
+//     res.send(`Update  user with id ${req.params.id}`)
+// })
+
+
+router.param("id", (req, res, next, id) => {
+    console.log(id);
+    next()
+});
+
+module.exports = router;
