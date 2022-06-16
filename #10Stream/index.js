458555fd7154bacf4917b2e36b9ff955a6606581
Omurbek Mamytbekov <omurbekmamytbekov545@gmail.com>
2022-06-16 12:07:39 +0600

    Pipe_Method

--- #10Stream/index.js
+++ #10Stream/index.js
@@ -5,16 +5,22 @@ const fs = require('fs')
 
 const readStream = fs.createReadStream('./doc/info.txt')
 
-readStream.on('data', (chunk) => {
-    console.log('=========');
-    console.log(chunk.toString());
-})
+// readStream.on('data', (chunk) => {
+//     console.log('=========');
+//     console.log(chunk.toString());
+// })
 
 
 // Эми биз жаза ала турган Stream жазабыз! Бул бизге жаны файлды тузууго жардам берет
 // Бул менен биз мурунку файлдагы данныйды жаны файлга кочуруп алдык
 const writeAtream = fs.createWriteStream('./doc/info_two_stream.txt')
 
-readStream.on('data', (chunk) => {
-    writeAtream.write(chunk)
-})
+// Method1
+// readStream.on('data', (chunk) => {
+//     writeAtream.write('\n===CHunk Stream')
+//     writeAtream.write(chunk)
+// })
+
+
+// Method2
+readStream.pipe(writeAtream)
