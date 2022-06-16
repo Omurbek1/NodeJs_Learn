6873b1af6bebda80ccc111c9670182ffa67afe9a
Omurbek Mamytbekov <omurbekmamytbekov545@gmail.com>
2022-06-16 11:37:00 +0600

    middleware

--- Express_Course/#4lesson_routes/index.js
+++ Express_Course/#4lesson_routes/index.js
@@ -3,12 +3,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const userRouter = require('./routes/user')
 
+app.use(express.static('public'));
 app.set('view engine', 'ejs')
-app.use(middleLogger)
-app.get('/', function (req, res) {
-    res.render('index', { title: "Oma" })
-})
-
 app.use('/users', userRouter)
 
 function middleLogger(req, res, next) {
